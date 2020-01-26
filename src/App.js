162a301bb3be45fd8108d0d97e9bc9355129337a
joseortiz9 <joseco04@live.com>
2020-01-27 00:59:19 +0300

    added router and shop page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from "react-router-dom";
+
 import './App.css';
+import HomePage from "./pages/homepage/homepage.comp";
+import ShopPage from "./pages/shop/shop.comp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop' component={ShopPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;

361a1856fb9f525885c65497c3d004ed2a5a4df9
joseortiz9 <joseco04@live.com>
2020-02-07 15:08:20 +0300

    added remove and add items functionality to the checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,4 @@
+
 export const addItemToCart = (actualCartItems, cartItemToAdd) => {
     const existingCartItem = actualCartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
@@ -11,4 +12,18 @@ export const addItemToCart = (actualCartItems, cartItemToAdd) => {
     /* always will add the quantity prop because the last if block
      will only run after we add the first item to the array */
     return [...actualCartItems, {...cartItemToAdd, quantity: 1}];
+};
+
+
+export const removeItemFromCart = (actualCartItems, cartItemToRemove) => {
+    const existingCartItem = actualCartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if (existingCartItem.quantity === 1)
+        return actualCartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    else {
+        return actualCartItems.map(cartItem =>
+            cartItem.id === cartItemToRemove.id ?
+                {...cartItem, quantity: cartItem.quantity - 1} :
+                cartItem);
+    }
 };

dad5cae7c03d3c24798a2db1fa0e3c5b8a814de2
joseortiz9 <joseco04@live.com>
2020-02-08 19:36:34 +0300

    styled-components applied to some comps for having a example

--- src/components/header/header.comp.jsx
+++ src/components/header/header.comp.jsx
@@ -6,33 +6,34 @@ import { createStructuredSelector } from "reselect";
 import {auth} from "../../firebase/firebase.utils";
 
 import {selectCurrentUser} from "../../redux/user/user.selectors";
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
 
 import CartIcon from "../cart-icon/cart-icon.comp";
 import CartDropDown from "../cart-dropdown/cart-dropdown.comp";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import './header.styles.scss'
-import {selectCartHidden} from "../../redux/cart/cart.selectors";
 
+//import './header.styles.scss'
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/about">ABOUT</Link>
-            <Link className="option" to="/contact">CONTACT</Link>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">SHOP</OptionLink>
+            <OptionLink to="/about">ABOUT</OptionLink>
+            <OptionLink to="/contact">CONTACT</OptionLink>
             {
                 currentUser ?
-                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    <OptionLink as="div" onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
                     :
-                    <Link className="option" to="/signin">SIGN IN</Link>
+                    <OptionLink to="/signin">SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         { hidden ? null : <CartDropDown /> }
-    </div>
+    </HeaderContainer>
 );
 
 

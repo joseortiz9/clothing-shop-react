1b0de3152718daa7871ddde1b7b2cbe043c11b57
joseortiz09 <joseco04@live.com>
2020-07-17 16:03:04 +0300

    starting over with new Computer

--- src/pages/Collection/collection.comp.jsx
+++ src/pages/Collection/collection.comp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-import {selectShopCollection} from "../../redux/shop/shop.selectors";
+import { selectShopCollection } from "../../redux/shop/shop.selectors";
 
 import CollectionItem from "../../components/collection-item/collection-item.comp";
 
